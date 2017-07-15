a6760a14e1774f44f817e6af93628a58464c2568
Jonas Bucher <j.bucher.mn@gmail.com>
2017-07-15 17:07:23 +0200

    Eigen

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,12 +2,19 @@
 #include <complex>
 #include <ctime>
 
+#include <Eigen/Dense>
+
 void matmul(int dim, const std::complex<double>* mat, const std::complex<double>* vec, std::complex<double>* res){
     for(int j=0; j<dim; j++){
         for(int i=0; i<dim; i++){
             res[i]+=mat[j*dim+i]*vec[j];
         }
     }
+}
+
+
+void matmul_Eigen(int dim, const std::complex<double>* mat, const std::complex<double>* vec, std::complex<double>* res){
+    Eigen::Map<Eigen::VectorXcd>(res, dim)+=Eigen::Map<Eigen::MatrixXcd>(const_cast<std::complex<double>*>(mat), dim, dim)*Eigen::Map<Eigen::VectorXcd>(const_cast<std::complex<double>*>(vec), dim);
 }
 
 
@@ -25,13 +32,28 @@ int main(int argc, char* argv[]){
     for(int i=0; i<dim;     i++) vec[i]=std::complex<double>(i,0.);
     for(int i=0; i<dim;     i++) res[i]=0;
 
-    clock_t t = clock();
-    for(int k=0; k<runs; k++) matmul(dim, mat, vec, res);
-    t = clock() - t;
-    double ns=1.e9*t/CLOCKS_PER_SEC;
 
+    clock_t t;
+    double ns;
 
     printf("Appc:     %9d\n",dim*dim);
+    
+    t = clock();
+    for(int k=0; k<runs; k++) matmul(dim, mat, vec, res);
+    t = clock() - t;
+    ns=1.e9*t/CLOCKS_PER_SEC;
+    
+    printf("------Simple--------\n");
+    printf("Time:     %12.2fns\n", ns/runs);
+    printf("Time/op:  %12.2fns\n", ns/(runs*dim*dim));
+
+    for(int i=0; i<dim;     i++) res[i]=0;
+    t = clock();
+    for(int k=0; k<runs; k++) matmul_Eigen(dim, mat, vec, res);
+    t = clock() - t;
+    ns=1.e9*t/CLOCKS_PER_SEC;
+
+    printf("------Eigen--------\n");
     printf("Time:     %12.2fns\n", ns/runs);
     printf("Time/op:  %12.2fns\n", ns/(runs*dim*dim));
 
